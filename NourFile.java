3289ee472c73cf7614961676e71f908d61bed2f1
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-03-11 15:21:08 +0200

    2 functions done

--- NourFile.java
+++ NourFile.java
@@ -1,40 +1,52 @@
-import java.util.Scanner;
+import java.util.*;
+import java.awt.Point;
 
 public class NourFile {
 
-    static byte[][] contructMatix(String[] photo, int team)
-    {
-        byte[][] matrix = new byte[photo.length][photo[0].length()] ;
-        
-        for(int i = 0; i < photo.length; i++){
-            for(int j = 0; j < photo[i].length(); j++) {
-                if(Character.compare(photo[i].charAt(j), (char)(team + '0')) == 0){
-                    matrix[i][j] = 1;
-                }
-                else{
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        return matrix;
-    }
+static void bubble_sort(ArrayList<Point> Centers) {
+    
+    for(int i = 0; i < Centers.size() ; i++) {
+        for(int j = 0; j < Centers.size()-1; j++) {
+            
+            if(Centers.get(j).getX() > Centers.get(j+1).getX()){
 
-    public static void main(String[] args) {
+                Point temp = Centers.get(j) ;
+                Centers.set(j , Centers.get(j+1)) ;
+                Centers.set(j+1 , temp) ;
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] photo = new String[n];
-        for(int i = 0; i < n; i++) {
-            photo[i] = sc.next();
-        }
-        int team = sc.nextInt();
-        byte[][] matrix = contructMatix(photo, team);
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+            }else if(Centers.get(j).getX() == Centers.get(j+1).getX() && 
+                    Centers.get(j).getY() > Centers.get(j+1).getY()) {
+
+                Point temp = Centers.get(j) ;
+                Centers.set(j , Centers.get(j+1)) ;
+                Centers.set(j+1 , temp) ;
+
             }
-            System.out.println();
+
         }
+
     }
+}
+
+public static void main(String[] args) {
+
+    ArrayList <Point> Centers = new ArrayList<>() ;
+    Centers.add(new Point(1 , 10)) ;
+    Centers.add(new Point(1 , 8)) ;
+    Centers.add(new Point(1 , 9)) ;
+    Centers.add(new Point(1 , 6)) ;
+    Centers.add(new Point(5 , 4)) ;
+    Centers.add(new Point(5 , 2)) ;
+    Centers.add(new Point(5 , 0)) ;
+    Centers.add(new Point(5 , 75)) ;
+    Centers.add(new Point(5 , 17)) ;
+    
+    bubble_sort(Centers) ;
+    System.out.println("ArrayList of Points:") ;
+
+    for (Point Center : Centers) {
+        System.out.println("X: " + Center.x + ", Y: " + Center.y) ;
+    }
+}
 
 }
