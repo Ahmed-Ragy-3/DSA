cf31bfe6a60a746f205fc4a311ea4c893112ae69
nourakram1 <nourakram94@gmail.com>
2024-03-21 00:25:46 +0200

    double

--- Labs/Lab4/Part1/DoubleLinkedList.java
+++ Labs/Lab4/Part1/DoubleLinkedList.java
@@ -23,49 +23,73 @@ public class DoubleLinkedList implements ILinkedList{
 
     @Override
     public void add(int index, Object element) {
-        
-        if(index < 0 || index >= size){
-            throw new IllegalStateException("Error") ;
-        }else if(index == size-1) {
-            add(element);
-            return ;
+        if(index < 0 || index > size)
+        {
+            System.out.println("Error") ;
+            System.exit(0) ;
         }
-
-        DoubleNode newNode = new DoubleNode(element) ;
-        
-        DoubleNode temp = head ;
-        for(int i = 0 ; i < index - 1 ; i++){
+        DoubleNode newNode = new DoubleNode(element);
+        if(index == 0)
+        {
+            addFirst(element);
+            return;
+        }
+        if(index == size)
+        {
+            add(element);
+            return;
+        }
+        DoubleNode temp = head;
+        for(int i = 0; i < index - 1; i++)
+        {
             temp = temp.getNext();
         }
-
-        newNode.setNext(temp.getNext()) ;
-        temp.setNext(newNode) ;
-        newNode.setPrev(temp) ;
-        //temp = temp.getNext().getNext() ;
-        newNode.getNext().setPrev(newNode) ;
-        size++ ;
-        
+        newNode.setNext(temp.getNext());
+        temp.setNext(newNode);
+        newNode.setPrev(temp);
+        temp = temp.getNext().getNext();
+        temp.setPrev(newNode);
+        size++;
+        return;
     }
-
+    public void addFirst(Object value) {
+        DoubleNode newNode = new DoubleNode(value);
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+            size++;
+            return;
+        }
+        newNode.setNext(head);
+        head.setPrev(newNode);
+        head = newNode;
+        size++;
+        return;
+    }
     @Override
     public void add(Object element) {
         
-        if(head == null && tail == null) {
-            DoubleNode new_Node = new DoubleNode(element , null , null) ;
-            head = new_Node ;
-            tail = new_Node ;
-        }else {
-            DoubleNode new_Node = new DoubleNode(element , tail , null) ;
-            tail = new_Node ;
+        if(tail == null) {
+            addFirst(element);
+            return;
         }
+        DoubleNode newNode = new DoubleNode(element);
+
+        tail.setNext(newNode);
+        newNode.setPrev(tail);
+        tail = newNode;
+        size++;
+        return;
     }
 
     @Override
     public Object get(int index) {
 
         if(index < 0 || index >= size) {
-            throw new IllegalStateException("There is no index " + index + ", the size is " + size);
+            System.out.println("Error") ;
+            System.exit(0) ;
         }
+        
         DoubleNode temp = head ;
 
         for(int i = 0 ; i < index ; i++) {
@@ -78,9 +102,10 @@ public class DoubleLinkedList implements ILinkedList{
     public void set(int index, Object element) {
         
         if(index < 0 || index >= size){
-            throw new IllegalStateException("There is no index = " + index + ", the size is " + size) ;
+            System.out.println("Error") ;
+            System.exit(0) ;
         }
-
+        
         DoubleNode temp = head ;
         for(int i = 0 ; i < index ; i++){
             temp = temp.getNext();
@@ -102,29 +127,41 @@ public class DoubleLinkedList implements ILinkedList{
         return (head == null)  ;
     }
 
+    public void remove() {
+        if (this.head != null) {
+            DoubleNode temp = this.head ;
+            this.head = this.head.getNext();
+            temp.setNext(null);
+            temp = null;
+            size--;
+        }
+    }
+
     @Override
-    
     public void remove(int index) {
-        
-        /*if(this.head == null || index >= size || index < 0) {
-            throw new IllegalStateException("Error") ;
-        }else if(index == 0) {
-            this.head = head.getNext() ;
-            return ;
+        if (this.head == null) {
+            System.out.println("Error");
+            System.exit(0);
         }
-
-        DoubleNode temp = this.head ;
-        //temp = this.head;
-        for(int i = 0; i < index-1; i++) {
-            temp = temp.getNext() ;
+        if (index == 0) {
+            this.remove();
+            return;
         }
-
-        DoubleNode temp2 = temp.getNext() ;
-        temp.setNext(temp2.getNext()) ;
-        //temp2.setNext(null);
-        temp2 = null ;
-        size-- ;*/
-    
+        DoubleNode temp = head;
+        if (index <= size - 1 && index >= 0) {
+            temp = this.head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.getNext();
+            }
+        } else {
+            System.out.println("Error");
+            System.exit(0);
+        }
+        DoubleNode temp2 = temp.getNext();
+        temp.setNext(temp2.getNext());
+        temp2.setNext(null);
+        temp2 = null;
+        size--;
     }
     
 
@@ -135,26 +172,22 @@ public class DoubleLinkedList implements ILinkedList{
 
     @Override
     public ILinkedList sublist(int fromIndex, int toIndex) {
-        if(fromIndex < 0 || toIndex > size || fromIndex > toIndex) {
-            throw new IllegalStateException("Error");
-        }else if(fromIndex == 0 && toIndex == size) {
-            return this ;
+        if (fromIndex < 0 || toIndex >= size || fromIndex > toIndex) {
+            System.out.println("Error");
+            System.exit(0);
         }
-
         DoubleNode temp = this.head ;
         DoubleNode temp2 = this.head ;
-
-        for(int i = 0 ; i < fromIndex ; i++) {
+        for (int i = 0; i < fromIndex; i++) {
             temp = temp.getNext();
         }
-
-        for(int i = 0 ; i < toIndex + 1 ; i++) {
+        for (int i = 0; i < toIndex + 1; i++) {
             temp2 = temp2.getNext();
         }
-
         DoubleLinkedList newList = new DoubleLinkedList();
-        
-        while(temp != temp2) {
+
+        while(temp != temp2)
+        {
             newList.add(temp.getValue()) ;
             temp = temp.getNext() ;
         }
@@ -163,15 +196,11 @@ public class DoubleLinkedList implements ILinkedList{
 
     @Override
     public boolean contains(Object o) {
-        if(isEmpty()) {
-            return false ;
-        }
-
         DoubleNode temp = head ;
         while (temp != null) {
-            if(temp.getValue() == o) {
+            if (temp.getValue() == o)
                 return true ;
-            }
+            temp = temp.getNext();
         }
         return false ;
     }
@@ -193,25 +222,96 @@ public class DoubleLinkedList implements ILinkedList{
         System.out.println(temp.getValue() + "]");
         //System.out.println();
     }
-    
-    public DoubleLinkedList take() {
 
+
+    public static void main(String[] args) {
+
+        DoubleLinkedList list = new DoubleLinkedList();
         Scanner input = new Scanner(System.in) ;
-        String str = input.nextLine() ;
-        input.close() ;
-        str.replaceAll("[|]", "") ;
-        String[] str_array = str.split(", ") ;
-
-        DoubleLinkedList list = new DoubleLinkedList() ;
-        for (String s : str_array) {
-            list.add(Integer.parseInt(s)) ;
+            
+            String str = input.nextLine().replaceAll("\\[|\\]", "");
+            if(str.compareTo("") == 0) {
+                list.clear();
+            }else{
+            String[] str_array = str.split(", ") ;
+            list.clear();
+            for (String s : str_array) {
+                list.add(Integer.parseInt(s));
+            }
+            
         }
-        return list ;
-    }
-
-    public void main(String[] args) {
-        DoubleLinkedList list1 = take() ;
-        list1.print() ;
+            
+            
+        str = input.nextLine();
+    
+        if(str.compareTo("isEmpty") == 0) {
+            if(list.isEmpty()) {
+                System.out.println("True");
+            }else {
+                System.out.println("False");
+            }
+            input.close();
+            return;
+        }else if(str.compareTo("add") == 0) {
+            int num = input.nextInt();
+            list.add(num);
+            list.print();
+            input.close();
+            return;
+        }else if(str.compareTo("addToIndex") == 0) {
+            int index = input.nextInt();
+            int num = input.nextInt();
+            list.add(index, num);
+            list.print();
+            input.close();
+            return;
+        }else if(str.compareTo("set") == 0) {
+            int index = input.nextInt();
+            int num = input.nextInt();
+            list.set(index, num);
+            list.print();
+            input.close();
+            return;
+        }else if(str.compareTo("get") == 0) {
+            int index = input.nextInt();
+            System.out.println(list.get(index));
+            input.close();
+            return;
+        }else if(str.compareTo("size") == 0) {
+            System.out.println(list.size());
+            input.close();
+            return;
+        }else if(str.compareTo("contains") == 0) {
+            int index = input.nextInt();
+            if(list.contains(index)){
+                System.out.println("True");
+            }else{
+                System.out.println("False");
+            }
+            input.close();
+            return;
+        }else if(str.compareTo("sublist") == 0) {
+            int fromIndex = input.nextInt();
+            int toIndex = input.nextInt();
+            DoubleLinkedList list2 = new DoubleLinkedList();
+            list2 = (DoubleLinkedList) list.sublist(fromIndex, toIndex);
+            list2.print();
+            input.close();
+            return;
+        }else if(str.compareTo("remove") == 0) {
+            
+            int index = input.nextInt();
+            list.remove(index);
+            list.print();
+            input.close();
+            return;
+        }else if(str.compareTo("clear") == 0) {
+            list.clear();
+            list.print();
+            
+        }
+        
+        input.close();
     }
     
 }
