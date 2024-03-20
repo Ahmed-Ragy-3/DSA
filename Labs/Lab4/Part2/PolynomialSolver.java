2877b00dc0c4a478359b2747cdb080bb6a79b5a8
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-03-21 00:28:13 +0200

    ay 7aga

--- Labs/Lab4/Part2/PolynomialSolver.java
+++ Labs/Lab4/Part2/PolynomialSolver.java
@@ -3,45 +3,48 @@ package Labs.Lab4.Part2;
 public class PolynomialSolver implements IPolynomialSolver {
 
     @Override
-    public void setPolynomial(char poly, int[][] terms) {
+    public static void setPolynomial(char poly, int[][] terms) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setPolynomial'");
     }
 
     @Override
-    public String print(char poly) {
+    public static String print(char poly) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'print'");
     }
 
     @Override
-    public void clearPolynomial(char poly) {
+    public static void clearPolynomial(char poly) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'clearPolynomial'");
     }
 
     @Override
-    public float evaluatePolynomial(char poly, float value) {
+    public static float evaluatePolynomial(char poly, float value) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'evaluatePolynomial'");
     }
 
     @Override
-    public int[][] add(char poly1, char poly2) {
+    public static int[][] add(char poly1, char poly2) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
 
     @Override
-    public int[][] subtract(char poly1, char poly2) {
+    public static int[][] subtract(char poly1, char poly2) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'subtract'");
     }
 
     @Override
-    public int[][] multiply(char poly1, char poly2) {
+    public static int[][] multiply(char poly1, char poly2) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'multiply'");
     }
     
+    public static void main(String[] args) {
+        
+    }
 }
